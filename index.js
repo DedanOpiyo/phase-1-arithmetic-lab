6569264954464b08e88a911949293d3866c5fd93
Dedan Opiyo <dedan.opiyo@student.moringaschool.com>
2025-03-20 22:08:50 +0300

    performs math operations using various operators and methods in JavaScript

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+
+// Create a variable called multiply set to an equation that will multiply the variables num1 and num2; the result of the multiplication should be 62.
+let multiply;
+let num1 = 2;
+let num2 = 31;
+multiply = (num1 * num2) // result is 32.
+
+// Create a variable called random that will generate a random integer greater than 0.
+let random;
+random = Math.floor((Math.random() * 10) + 1);
+
+// Create a variable called mod set to an equation that will calculate the remainder of dividing variable num3 by num4; the remainder should be 4.
+let mod;
+let num3 = 14;
+let num4 = 10;
+mod = num3 % num4;
+
+// Create a variable called max that finds the highest number in a set; the value returned should be 20.
+let max = Math.max(1, 19, 4, 14, 13, 19, 10, 20);
